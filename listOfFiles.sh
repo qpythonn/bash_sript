512657ce453e30eb6454602fcdfcf428b89dccc0
quentin <quentin.python@vub.ac.be>
2015-06-08 12:01:21 +0200

    fixes text

--- listOfFiles.sh
+++ listOfFiles.sh
@@ -1,8 +1,8 @@
-# This is a small macro to list all the file contained in a directory.
+# This is a small macro to list all the files contained in a directory.
 #Syntax:
 # source listOfFiles.sh arg1 arg2
 # arg1 is the path to the directory in which we want to ls
-# arg2 is an optionnal argument allowing to search for file containing "arg2" in their nams 
+# arg2 is an optionnal argument allowing to search for file containing "arg2" in their names 
 #
 
 

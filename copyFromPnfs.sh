80223d7015034e63a5d755f4ef4f0481f34a9de7
quentin <quentin.python@vub.ac.be>
2015-06-08 15:06:21 +0200

    adds the option to merge all the root files using hadd

--- copyFromPnfs.sh
+++ copyFromPnfs.sh
@@ -3,7 +3,7 @@
 # arg1 is the path to the directory from which we want to copy
 # arg2 is the path to the directory in which we want to copy
 # arg3 is the string that each file to be copied should contained in their names (typically "root")
-
+# arg4 one set to "merge" hadd will be called for all root files in the $2 directory
 
 # first creat the list of command
 source createListOfCmd.sh $1 $2 $3 > preAwk.txt
@@ -20,3 +20,15 @@ source postawk.txt
 # move the txt files to the same location than the root files
 mv preAwk.txt $2
 mv postawk.txt $2
+
+if [[ -n $4 ]] #check if variable is not empty
+then
+    if [[ $4 == "merge" ]]
+    then
+	cd $2
+	echo "merging all root files using hadd"
+	hadd all.root *.root
+	cd -
+    fi
+fi
+

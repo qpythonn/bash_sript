7caccc0a6d23a935c6a73ab9904b44d234dfdff9
quentin <quentin.python@vub.ac.be>
2015-06-08 11:57:27 +0200

    first commit of script to list files in a given directory

---
+++ listOfFiles.sh
@@ -0,0 +1,31 @@
+# This is a small macro to list all the file contained in a directory.
+#Syntax:
+# source listOfFiles.sh arg1 arg2
+# arg1 is the path to the directory in which we want to ls
+# arg2 is an optionnal argument allowing to search for file containing "arg2" in their nams 
+#
+
+
+echo "Printing the list of file in Directory ..."
+Dir1=$1
+echo $1
+echo "with search pattern ... "
+
+
+if [[ $2 ]]
+then
+    echo "pattern is " $2
+    for file in `ls $1 | grep $2`
+    do
+	echo $file
+    done    
+fi
+
+if [[ ! $2 ]]
+then
+    echo "no pattern"
+    for file in `ls $1`
+    do
+	echo $file
+    done    
+fi
